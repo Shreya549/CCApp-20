1640c1aa509ba9d81e00eea5e4589e23f43e4e45
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-06-26 01:46:39 +0530

    Registration Number Validation added

--- Accounts/views.py
+++ Accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 import jwt, requests
 from .models import User
+from Members.models import Members
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, serializers
@@ -15,6 +16,11 @@ class UserRegistration(APIView):
     serializer_class = UserRegistrationSerializer
 
     def post(self, request):
+
+        regno = request.data['regno']
+        query = Members.objects.filter(regno = regno)
+        if not query.exists():
+            return Response('Invalid Registration Number Entered', status = 403)
         serializer = self.serializer_class(data=request.data)
         try:
             serializer.is_valid(raise_exception = True)
@@ -30,7 +36,6 @@ class UserLogin(APIView):
     serializer_class = UserLoginSerializer
 
     def post(self, request):
-
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
