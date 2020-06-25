9bcc7270192fc175c5dd23c0ab5f52301a87de89
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-06-26 00:51:38 +0530

    Minor bug

--- Members/urls.py
+++ Members/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import ListUploadViewSet, MembersView, MembersListViewSet
 
 router = DefaultRouter()
-router.register('upload/', ListUploadViewSet, basename = 'upload')
-router.register('view/', MembersListViewSet, basename = 'view')
+router.register('upload', ListUploadViewSet, basename = 'upload')
+router.register('view', MembersListViewSet, basename = 'view')
 urlpatterns = router.urls
 
 urlpatterns += [
