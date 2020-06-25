582dcd9692e5d33f38dc06d9975b56d11dbeecc8
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-06-25 20:38:38 +0530

    Models added

--- CC20App/settings.py
+++ CC20App/settings.py
@@ -138,4 +138,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())
---
+++ Members/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from Accounts.models import User
+from uuid import uuid4
+import uuid, os
+
+def path_and_rename(instance, filename):
+    upload_to = 'members'
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    else:
+        # set filename as random string
+        filename = '{}.{}'.format(uuid4().hex, ext)
+    # return the whole path to the file
+    return os.path.join(upload_to, filename)
+
+# Create your models here.
+class MembersListFile(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key = True)
+    owner = models.ForeignKey(User, on_delete = models.CASCADE, related_name='member')
+    csv = models.FileField(upload_to=path_and_rename)
