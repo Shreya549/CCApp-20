d714a2c4e24691be094cc5b7acc57d1f0db10c81
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-07-03 19:00:53 +0530

    Bill model added

--- Reimbursements/models.py
+++ Reimbursements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from Accounts.models import User
+from uuid import uuid4
 
 # Create your models here.
+class Bill(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key = True)
+    owner = models.ForeignKey(User, on_delete = models.CASCADE, related_name='member_list')
+    name = models.CharField(max_length = 100)
+    bill = models.ImageField()
+    remarks = models.TextField()
+    status = models.CharField(max_length = 200)
