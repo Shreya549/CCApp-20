79adcba5e3a39650b9791d3da93e5e1dcdaa2421
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-07-04 22:14:02 +0530

    Views added for Reimbursements

--- Reimbursements/models.py
+++ Reimbursements/models.py
@@ -4,12 +4,26 @@ from uuid import uuid4
 import uuid
 
 # Create your models here.
+
+def path_and_rename(instance, filename):
+    upload_to = 'bills'
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    else:
+        # set filename as random string
+        filename = '{}.{}'.format(uuid4().hex, ext)
+    # return the whole path to the file
+    return os.path.join(upload_to, filename)
+
+
 class Bill(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, primary_key = True)
     owner = models.ForeignKey(User, on_delete = models.CASCADE, related_name='bill')
     name = models.CharField(max_length = 100)
-    bill = models.ImageField()
-    remarks = models.TextField(null = True)
+    bill = models.ImageField(upload_to = path_and_rename, null = True)
+    remarks = models.TextField(null = True) # By person who will be paid
 
     NOT_REVIEWED = 'Not Reviewed'
     PAID = 'Paid'
@@ -25,4 +39,4 @@ class Bill(models.Model):
         default = NOT_REVIEWED
     )
 
-    comments = models.CharField(max_length=200, default = '-')
+    comments = models.CharField(max_length=200, default = '-') # By Finance Director
--- Reimbursements/views.py
+++ Reimbursements/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics
+from .serializers import BillSerializers
+from .models import Bill
 
 # Create your views here.
+class BillViewsets(viewsets.ModelViewSet):
+
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = BillSerializers
+
+    def get_queryset(self):
+        myBill = Bill.objects.filter(owner = self.request.user)
+        return (myBill)
+
+    def perform_create(self, serializer):
+        serializer.save(owner = self.request.user)
