bfbebd65d5b4c36dda5075608d61799f8cd4c923
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-07-05 15:08:06 +0530

    Bugs fixed

--- Meeting/views.py
+++ Meeting/views.py
@@ -17,9 +17,6 @@ class MeetingViewSet(viewsets.ModelViewSet):
         return self.update(request, *args, **kwargs)
 
     def perform_create(self, serializer):
-        try :
-            category = Members.objects.get(regno = (self.request.user.regno)).category
-            if (category == 3 or category == 5):
-                serializer.save(owner=self.request.user)
-        except:
-            pass
+        category = Members.objects.get(regno = (self.request.user.regno)).category
+        if (category == 3 or category == 5):
+            serializer.save(owner=self.request.user)
