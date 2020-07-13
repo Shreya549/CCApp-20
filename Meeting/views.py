62b925982067591f212dc23bb57792e0004fe912
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-07-13 23:32:11 +0530

    Views changed

--- Meeting/views.py
+++ Meeting/views.py
@@ -47,7 +47,7 @@ class MarkAttendanceViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         meeting = self.request.GET.get('meeting')
         regno = self.request.user.regno
-        attendance = Attendance.objects.filter(meeting=meeting, regno = regno).values_list('uuid', flat=True)[0]
+        attendance = Attendance.objects.filter(meeting=meeting, regno = regno).values_list('uuid', flat=True)
         attn = Attendance.objects.get(pk=attendance)
         attn.isPresent = True
         attn.save()
