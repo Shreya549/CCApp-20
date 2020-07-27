dc2bbaa0e5a2b156b617ff023a8c230b2f4c3fab
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2020-07-27 16:38:18 +0530

    Update bills added

--- Reimbursements/urls.py
+++ Reimbursements/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import BillViewsets
+from .views import BillViewsets, ViewBillViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('new', BillViewsets, basename = 'new')
+router.register('view', ViewBillViewSet, basename = 'view')
 urlpatterns = router.urls
--- Reimbursements/views.py
+++ Reimbursements/views.py
@@ -18,3 +18,33 @@ class BillViewsets(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner = self.request.user, name = self.request.user.name)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=204)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+
+class ViewBillViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = BillSerializers
+
+    def get_queryset(self):
+        bill = Bill.objects.all()
+        return (bill)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs) 
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=204)
+
+    def perform_destroy(self, instance):
+        instance.delete()
